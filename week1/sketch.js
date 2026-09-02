0dd90226010c890620d3c97477f5a55e068d3a6b
Scott Fitzgerald <scott@droolcup.com>
2026-09-02 08:54:10 -0400

    updated repo for f26 assignments

--- week1/sketch.js
+++ week1/sketch.js
@@ -1,10 +1,55 @@
+// change the quotes in this array. Be mindful of the quotation marks!
+const quotes = [
+  { text: "the intersection of programming, design, and art", source: "Scott" },
+  { text: "a chance for code to be wrong", source: "Sara" },
+  { text: "surprise, exploration, and aesthetic exploration", source: "Sally" },
+  { text: "process over results", source: "Sam" },
+  { text: "form over function", source: "Sean" },
+  { text: "iterative and experimental", source: "Scooby" },
+  { text: "a community of practice", source: "Seth" },
+  { text: "an opportunity to stick it to the man", source: "Shifty-eyed Susan" }
+];
+
+// a variable tht holds the current quote
+let current = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  randomSeed(millis());
+  pickQuote(); // calls the function to pick a quote
+}
+
+function pickQuote() {
+  // take a random number and use that to identify what quote to use
+  current = quotes[floor(quotes.length)]; 
 }
 
 function draw() {
-  stroke(255);
-  noFill();
-  circle(mouseX,mouseY,25);
+  background(255, 20, 250);
+  drawQuote(); // draw the quote on screen
+}
+
+function drawQuote() {
+  // draw text
+  fill(10, 255, 10);
+  textAlign(CENTER, CENTER);
+  text("Creative Coding is.....", width / 2, height / 2 - 48);
+  textSize(32);
+  textStyle(BOLD);
+  text("“" + current.text + "”", width / 2, height / 2);
+  textAlign(RIGHT, CENTER);
+  text("-" + current.source, width - 100, height - 100);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function newQuote() {
+  pickQuote();
+  redraw();
+}
+
+function mousePressed() {
+  newQuote(); /
 }
